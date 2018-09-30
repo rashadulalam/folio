a08613bdd53cf30522a52bf6dab01996382b03d6
rashadulalam <rashadul91@gmail.com>
2018-09-30 07:30:31 +0600

    Header & footer file created separated from index file

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!-- Bootstrap core JavaScript -->
+    <script src="<?php echo get_template_directory_uri(); ?>/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo get_template_directory_uri(); ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo get_template_directory_uri(); ?>/js/resume.min.js"></script>
+
+  </body>
+
+</html>
